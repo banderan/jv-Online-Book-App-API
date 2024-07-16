458683df1915bb3f4de54d0ce3f705464688ea3f
Banderan <bartoszwojcik2004@gmail.com>
2024-07-16 21:16:48 +0200

    fixing import in repository

--- src/main/java/org/example/jvspringbootfirstbook/repository/BookRepositoryImpl.java
+++ src/main/java/org/example/jvspringbootfirstbook/repository/BookRepositoryImpl.java
@@ -5,7 +5,6 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import java.util.List;
 import java.util.Optional;
