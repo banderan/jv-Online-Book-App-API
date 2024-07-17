8f43e6993c4b0e8bcfa9bbb88234b6c7a51cafd8
Banderan <bartoszwojcik2004@gmail.com>
2024-07-17 19:58:51 +0200

    mvn clean package -> Spring Boot Portfolio Project — Part 3

--- src/main/java/org/example/jvspringbootfirstbook/controler/BookController.java
+++ src/main/java/org/example/jvspringbootfirstbook/controler/BookController.java
@@ -6,7 +6,14 @@ import org.example.jvspringbootfirstbook.dto.BookDto;
 import org.example.jvspringbootfirstbook.dto.CreateBookRequestDto;
 import org.example.jvspringbootfirstbook.service.BookService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
--- src/main/java/org/example/jvspringbootfirstbook/model/Book.java
+++ src/main/java/org/example/jvspringbootfirstbook/model/Book.java
@@ -32,5 +32,5 @@ public class Book {
     private String coverImage;
 
     @Column(nullable = false)
-    private boolean is_deleted = false;
+    private boolean isDeleted = false;
 }
