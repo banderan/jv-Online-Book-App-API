ed7ffb0e4ddda59b951f176923d14a9c2288a298
Banderan <bartoszwojcik2004@gmail.com>
2024-07-30 19:53:27 +0200

    fixing register method from userRepository

--- src/main/java/org/example/jvspringbootfirstbook/repository/role/RoleRepository.java
+++ src/main/java/org/example/jvspringbootfirstbook/repository/role/RoleRepository.java
@@ -1,7 +1,12 @@
 package org.example.jvspringbootfirstbook.repository.role;
 
 import org.example.jvspringbootfirstbook.model.Role;
+import org.example.jvspringbootfirstbook.model.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface RoleRepository extends JpaRepository<Role, Long> {
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long>, JpaSpecificationExecutor<Role> {
+    Optional<Role> findByName(RoleName name);
 }
