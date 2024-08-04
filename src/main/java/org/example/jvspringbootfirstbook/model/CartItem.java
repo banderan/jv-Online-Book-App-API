c2e9cc76135a3c5c89b28620e015e038718bcd90
Banderan <bartoszwojcik2004@gmail.com>
2024-08-04 21:57:25 +0200

    adding liquidbase

--- src/main/java/org/example/jvspringbootfirstbook/model/CartItem.java
+++ src/main/java/org/example/jvspringbootfirstbook/model/CartItem.java
@@ -1,6 +1,13 @@
 package org.example.jvspringbootfirstbook.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
@@ -17,8 +24,8 @@ public class CartItem {
     @ManyToOne
     @JoinColumn(name = "cart_id", nullable = false)
     private ShoppingCart shoppingCart;
-    @Column(nullable = false)
     @ManyToOne
+    @JoinColumn(name = "books_id", nullable = false)
     private Book book;
     @Column(nullable = false)
     private int quantity;
--- src/main/java/org/example/jvspringbootfirstbook/model/ShoppingCart.java
+++ src/main/java/org/example/jvspringbootfirstbook/model/ShoppingCart.java
@@ -1,11 +1,18 @@
 package org.example.jvspringbootfirstbook.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+import java.util.Set;
 import lombok.Data;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
-
-import java.util.Set;
 
 @Entity
 @Data
@@ -16,8 +23,8 @@ public class ShoppingCart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
     @OneToOne
+    @PrimaryKeyJoinColumn
     private User user;
     @OneToMany(mappedBy = "shoppingCart")
     private Set<CartItem> cartItems;
