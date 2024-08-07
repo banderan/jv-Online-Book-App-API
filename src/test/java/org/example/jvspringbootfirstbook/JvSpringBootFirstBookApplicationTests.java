bf48fccfadd71059e1d53abbbb24e11c3f9394ac
Banderan <bartoszwojcik2004@gmail.com>
2024-08-07 17:55:02 +0200

    something go wrong with docker

--- src/test/java/org/example/jvspringbootfirstbook/JvSpringBootFirstBookApplicationTests.java
+++ src/test/java/org/example/jvspringbootfirstbook/JvSpringBootFirstBookApplicationTests.java
@@ -1,13 +1,8 @@
 package org.example.jvspringbootfirstbook;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JvSpringBootFirstBookApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
